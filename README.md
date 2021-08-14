3d631880d53bdb29b72f85ad82798f394e4ae015
tapan021299 <77850787+tapan021299@users.noreply.github.com>
2021-08-15 01:32:09 +0530

    added UI, styling and app login

---
+++ README.md
@@ -0,0 +1,16 @@
+# Stock Mood Checker
+![](https://github.com/tap0212/stocks-app/blob/main/demo.gif)
+
+### Problem Statement
+- Take price of purchase, number of shares and current price
+- Show profit/loss made 
+
+### Tech Stack Used
+- HTML, CSS and Js
+- Webpack for bundling and routing
+
+### Key features
+- Form Validation
+- Toast messages
+- Theme change on loss or profit accordingly (Check GIF)
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,134 @@
 import "../css/styles.css";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+
+const getValueById = (id) => document.getElementById(id).value;
+const calculatePNL = (
+    priceOfPurchase,
+    numberOfSharesPurchased,
+    currentPrice
+) => {
+    if (!priceOfPurchase || !numberOfSharesPurchased || !currentPrice) {
+        return;
+    }
+    const totalPriceOfPurchase = priceOfPurchase * numberOfSharesPurchased;
+    const totalCurrentPrice = currentPrice * numberOfSharesPurchased;
+    const profitOrLossAmount = totalCurrentPrice - totalPriceOfPurchase;
+    if (!profitOrLossAmount) {
+        Toastify({
+            text: "Everything's under control! There's no loss or profit",
+            duration: 3000,
+            close: true,
+            gravity: "top",
+            position: "right",
+            backgroundColor: "#415d43",
+            stopOnFocus: true,
+            onClick: function () {},
+        }).showToast();
+        return;
+    }
+    const profitOrLossPercentage =
+        (profitOrLossAmount / totalPriceOfPurchase) * 100;
+    return {
+        profitOrLossAmount,
+        profitOrLossPercentage: profitOrLossPercentage.toFixed(2),
+    };
+};
+const removeClassNames = (elementAndClassNameList) => {
+    elementAndClassNameList.forEach((e) => {
+        console.log({ e });
+        e.element.classList.remove(e.class);
+    });
+};
+const addClassNames = (elementAndClassNameList) => {
+    elementAndClassNameList.forEach((e) => {
+        console.log({ e });
+        e.element.classList.add(e.class);
+    });
+};
+
+(() => {
+    document.getElementById("submit-btn").addEventListener("click", () => {
+        const priceOfPurchase = getValueById("price-of-purchase");
+        const numberOfSharesPurchased = getValueById("number-of-shares");
+        const currentPrice = getValueById("price-currently");
+        const wrapper = document.getElementById("wrapper");
+        const outputText = document.getElementById("output");
+        removeClassNames([
+            {
+                element: wrapper,
+                class: "success",
+            },
+            {
+                element: wrapper,
+                class: "failure",
+            },
+            {
+                element: outputText,
+                class: "success-text",
+            },
+            {
+                element: outputText,
+                class: "failure-text",
+            },
+        ]);
+
+        outputText.innerText = "";
+        // validations
+        document.getElementById("error-price-of-purchase").innerText = "";
+        document.getElementById("error-number-of-shares").innerText = "";
+        document.getElementById("error-price-currently").innerText = "";
+        if (!priceOfPurchase) {
+            document.getElementById("error-price-of-purchase").innerText =
+                "Please enter a valid purchase amount";
+        }
+        if (!numberOfSharesPurchased) {
+            document.getElementById("error-number-of-shares").innerText =
+                "Please enter valid number of shares";
+        }
+        if (!currentPrice) {
+            document.getElementById("error-price-currently").innerText =
+                "Please enter a valid amount";
+        }
+
+        const profitOrLoss = calculatePNL(
+            priceOfPurchase,
+            numberOfSharesPurchased,
+            currentPrice
+        );
+        console.log(Math.sign(profitOrLoss.profitOrLossAmount));
+        if (Math.sign(profitOrLoss.profitOrLossAmount) === 1) {
+            addClassNames([
+                {
+                    element: wrapper,
+                    class: "success",
+                },
+                {
+                    element: outputText,
+                    class: "success-text",
+                },
+            ]);
+            outputText.innerText = `Woah! You made profit of ${Math.abs(
+                profitOrLoss.profitOrLossAmount
+            )} units i.e. ${Math.abs(
+                profitOrLoss.profitOrLossPercentage
+            )}% 🤑🤩`;
+        } else {
+            addClassNames([
+                {
+                    element: wrapper,
+                    class: "failure",
+                },
+                {
+                    element: outputText,
+                    class: "failure-text",
+                },
+            ]);
+            outputText.innerText = `Oh snap! You're in loss of ${Math.abs(
+                profitOrLoss.profitOrLossAmount
+            )} units i.e. ${Math.abs(
+                profitOrLoss.profitOrLossPercentage
+            )}% 😟😵‍💫`;
+        }
+    });
+})();
